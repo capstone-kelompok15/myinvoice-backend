15d4530e509ef1bebf4181a02e8a0811643626f2
Yudistira Maulana <yudistir4maulana@gmail.com>
2022-12-14 19:30:31 +0700

    [feat] get unread notif count

--- cmd/webservice/customer/router/router.go
+++ cmd/webservice/customer/router/router.go
@@ -29,6 +29,7 @@ func InitCustomerRouter(params *RouterParams) {
 	customerV1Group.GET("/me", customerHandler.GetCustomerDetails(), params.Middleware.CustomerMustAuthorized())
 	customerV1Group.GET("", customerHandler.GetAllCustomer(), params.Middleware.AdminMustAuthorized())
 	customerV1Group.GET("/notifications", customerHandler.GetAllNotificationCustomer(), params.Middleware.CustomerMustAuthorized())
+	customerV1Group.GET("/notifications/unread_count", customerHandler.GetUnreadNotifCount(), params.Middleware.CustomerMustAuthorized())
 	customerV1Group.PATCH("/me/picture", customerHandler.UpdateCustomerProfilePicture(), params.Middleware.CustomerMustAuthorized())
 	customerV1Group.PUT("/me", customerHandler.UpdateCustomer(), params.Middleware.CustomerMustAuthorized())
 	customerV1Group.PUT("/notifications/:id", customerHandler.MarkNotifCustomerAsRead(), params.Middleware.CustomerMustAuthorized())
